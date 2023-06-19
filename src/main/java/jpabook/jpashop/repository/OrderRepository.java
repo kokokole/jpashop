d002585b0868f2450c14ad2ec69a25da30ebea0f
James <emuext@gmail.com>
2023-06-19 23:33:40 +0900

    2023-06-19 주문조회 최적화 진행 fetch join 적용

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -52,4 +52,11 @@ public class OrderRepository {
     }
 
 
+    public List<Order> findWithMemberDelivery() {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class).getResultList();
+
+    }
 }
